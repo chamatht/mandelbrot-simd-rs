82dd73095a51b16ce56d2e05bcb7b05a3a3ad1c9
chamatht <chamatht@gmail.com>
2019-09-13 18:03:22 +0200

    refactor names

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,13 @@ fn main() {
     const IMG_WIDTH: i32 = 1024;
     const IMG_HEIGHT: i32 = 1024;
     let max_iterations = 32;
-    let cmin_x: f32 = -2.0;
-    let cmax_x: f32 = 1.0;
-    let cmin_y: f32 = -1.5;
-    let cmax_y: f32 = 1.5;
+    let cmin_r: f32 = -2.0;
+    let cmax_r: f32 = 1.0;
+    let cmin_i: f32 = -1.5;
+    let cmax_i: f32 = 1.5;
 
-    let scale_x = (cmax_x - cmin_x) / (IMG_WIDTH as f32);
-    let scale_y = (cmax_y - cmin_y) / (IMG_HEIGHT as f32);
+    let scale_x = (cmax_r - cmin_r) / (IMG_WIDTH as f32);
+    let scale_y = (cmax_i - cmin_i) / (IMG_HEIGHT as f32);
     let iter_scale = 255 / max_iterations;
 
     let mut image: Vec<u8> = vec![0; (IMG_WIDTH * IMG_HEIGHT * 3) as usize];
@@ -34,24 +34,24 @@ fn main() {
                     xf + 6.0,
                     xf + 7.0,
                 );
-                let cx = cmin_x + (vx * scale_x);
-                let cy = cmax_y - (f32x8::splat(y as f32) * scale_y);
-                let mut zx = cx;
-                let mut zy = cy;
+                let c_r = cmin_r + (vx * scale_x);
+                let c_i = cmax_i - (f32x8::splat(y as f32) * scale_y);
+                let mut z_r = c_r;
+                let mut z_i = c_i;
 
                 let mut ret = i32x8::splat(1);
                 let mask = m32x8::splat(true);
 
                 for _ in 0..max_iterations {
-                    let mut zx2 = zx * zx;
-                    let mut zy2 = zy * zy;
-                    let zxzy = zx * zy;
-                    zx = mask.select(zx2 - zy2 + cx, zx);
-                    zy = mask.select(zxzy + zxzy + cy, zy);
+                    let mut z_r2 = z_r * z_r;
+                    let mut z_i2 = z_i * z_i;
+                    let zrzi = z_r *z_i;
+                    z_r = mask.select(z_r2 - z_i2 + c_r, z_r);
+                    z_i = mask.select(zrzi + zrzi + c_i, z_i);
 
-                    zx2 = zx * zx;
-                    zy2 = zy * zy;
-                    let mask = (zx2 + zy2).le(f32x8::splat(4.0));
+                    z_r2 = z_r * z_r;
+                    z_i2 = z_i * z_i;
+                    let mask = (z_r2 +z_i2).le(f32x8::splat(4.0));
                     ret = mask.select(ret + 1, ret);
 
                     if mask.none() {
@@ -59,11 +59,11 @@ fn main() {
                     }
                 }
 
-                for i in 0i32..8 {
-                    let j = (255 - (ret.extract(i as usize) * iter_scale) % 255) as u8;
-                    let index = ((y * IMG_WIDTH + x + i) * 3) as usize;
+                for c in 0i32..8 {
+                    let color = (255 - (ret.extract(c as usize) * iter_scale) % 255) as u8;
+                    let index = ((y * IMG_WIDTH + x + c) * 3) as usize;
                     image[index] = (255 * x / IMG_HEIGHT) as u8;
-                    image[index + 1] = j;
+                    image[index + 1] = color;
                     image[index + 2] = (255 * y / IMG_WIDTH) as u8;
                 }
             }
